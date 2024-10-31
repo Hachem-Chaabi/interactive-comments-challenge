7ffebdbfc16b48c291575fb6fd601d0aae949c84
Hachem Chaabi <hachemchaabi57@gmail.com>
2024-10-31 16:42:23 +0100

    Implementing the vote and reply features

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 'use strict';
 
-function fetchJSONData() {
-    fetch("./data.json")
-        .then((res) => {
-            if (!res.ok) {
-                throw new Error
-                    (`HTTP error! Status: ${res.status}`);
-            }
-            return res.json();
-        })
-        .then((data) =>
-            console.log(data))
-        .catch((error) =>
-            console.error("Unable to fetch data:", error));
-}
-fetchJSONData();
+// Get data from .JSON file
+import data from './data.json' with { type: 'json' };
+console.log(data);
+
+// Variables
+const plusBtn = document.querySelectorAll('.plus-btn');
+const minusBtn = document.querySelectorAll('.minus-btn');
+const replyBtn = document.querySelectorAll('.reply-container');
+const user = data.comments;
+// console.log(user[0].score);
+
+// Adding 1 to the vote
+plusBtn.forEach( btn => {
+    btn.addEventListener('click', () => {
+        const dataNum = btn.dataset.num;
+        const intercationNumber = document.querySelector(`.interaction-number-${dataNum}`);
+        intercationNumber.textContent = Number(intercationNumber.textContent) + 1;
+    });
+});
+
+// Subtract 1 from the vote
+minusBtn.forEach( btn => {
+    btn.addEventListener('click', () => {
+        const dataNum = btn.dataset.num;
+        const intercationNumber = document.querySelector(`.interaction-number-${dataNum}`);
+        intercationNumber.textContent = Number(intercationNumber.textContent) - 1;
+    });
+});
+
+// Reply
+replyBtn.forEach( btn => {
+    btn.addEventListener('click', () => {
+        const d = btn.closest('.when__reply');
+        const element = document.getElementById("add-reply");
+
+        if (typeof(element) != 'undefined' && element != null) return;
+        console.log(d);
+
+        const html = `<div class="add__comment-container">
+          <img src="images/avatars/image-juliusomo.webp" alt="student wearing glasses">
+          <textarea class="textarea" placeholder='Add a comment...' name="add-reply" id="add-reply"></textarea>
+          <button>REPLY</button>
+        </div>`;
+        d.insertAdjacentHTML('beforeend', html);
+    });
+});
